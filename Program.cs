221cea66323aa350ce76ec7583c0bf4452adfd7d
Mark Roger <markroger@Marks-iMac.local>
2022-10-27 02:07:10 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using dotnet_dictionary;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
